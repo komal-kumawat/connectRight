ae01fb52bc10416178488b0c2194bf9941f376bb
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-09-06 02:17:45 +0530

    adding middleware and limiting the payload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb" , extended:true}));
 
 // connecting websocket server
 const io = ConnectToSocket(server , {
@@ -29,7 +30,7 @@ mongoose.connect(process.env.MONGO_URL as string)
 })
 
 
-app.use("/")
+// app.use("/api/v1/users" , userRoutes);
 
 const port =process.env.PORT ;
 server.listen(port , ()=>{
